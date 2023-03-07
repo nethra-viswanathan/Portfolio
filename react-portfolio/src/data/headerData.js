41991b11b01338c318d9d154ed1bd242744b793b
nethraviswanathan29 <113320191+nethraviswanathan29@users.noreply.github.com>
2023-03-07 12:13:00 -0500

    changing intro

--- react-portfolio/src/data/headerData.js
+++ react-portfolio/src/data/headerData.js
@@ -3,7 +3,7 @@ import photo from '../assets/imgs/LinkedIn photo.jpeg'
 export const headerData = {
     name: 'Nethra Viswanathan',
     title: "Web Developer",
-    desciption:"I‘ve come to embrace my overthinking, awkward self and have been able to make this work in my favour (at least in the workspace). I push my limits, always look for ways to do better and by now, I’ve perfected the art of doing this without driving anyone up the wall",
+    desciption:"I am passionate about creating software and want to use technology to help make people's lives more fulfilling. I thrive in teams and always looking for something exciting to learn.",
     image: photo,
     resumePdf: resume
 }
