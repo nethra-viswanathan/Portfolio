068fa870d9d44573b7b325fa552babad90b2f12a
nethraviswanathan29 <113320191+nethraviswanathan29@users.noreply.github.com>
2023-04-03 14:14:33 -0400

    made it iphone responsive

--- react-portfolio/src/data/headerData.js
+++ react-portfolio/src/data/headerData.js
@@ -3,7 +3,7 @@ import photo from '../assets/imgs/LinkedIn photo.jpeg'
 export const headerData = {
     name: 'Nethra Viswanathan',
     title: "Web Developer",
-    desciption:"I am passionate about creating software and want to use technology to help make people's lives more fulfilling. I thrive in teams and always looking for something exciting to learn.",
+    desciption:"I am passionate about creating software and want to use technology to help make people's lives more fulfilling. I thrive in teams and I'm always looking for something exciting to learn.",
     image: photo,
     resumePdf: resume
 }
